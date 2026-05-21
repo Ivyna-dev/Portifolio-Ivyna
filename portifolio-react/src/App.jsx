829fc3d9df2d395b3034b632f99efccc5f44725a
ivyna-dev <ivynasrodrigues@gmail.com>
2026-05-21 10:24:06 -0300

    feat: setup inicial e background animado

---
+++ portifolio-react/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css"
+import StarBackground from "./components/StarBackground"
+
+function App() {
+  return (
+    <>
+      <StarBackground />
+
+      <main>
+        <h1>Ivyna Sousa</h1>
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ portifolio-react/src/components/StarBackground.jsx
@@ -0,0 +1,106 @@
+import Particles from "react-tsparticles"
+import { loadFull } from "tsparticles"
+import { useCallback } from "react"
+
+export default function StarBackground() {
+  const particlesInit = useCallback(async (engine) => {
+    await loadFull(engine)
+  }, [])
+
+  return (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      options={{
+        background: {
+          color: "#070816",
+        },
+
+        fullScreen: {
+          enable: true,
+          zIndex: -1,
+        },
+
+        fpsLimit: 60,
+
+        interactivity: {
+          events: {
+            onHover: {
+              enable: true,
+              mode: "bubble",
+            },
+          },
+
+          modes: {
+
+            bubble: {
+              distance: 90,
+              opacity: 1.5,
+              size: 3,
+              duration: 2,
+            },
+          },
+        },
+
+        particles: {
+          links: {
+            enable: false,
+          },
+
+          collisions: {
+            enable: false,
+          },
+
+          number: {
+            value: 112,
+          },
+
+          color: {
+            value: ["#ffffff", "#c4b5fd", "#a78bfa"],
+          },
+
+          opacity: {
+            value: {
+              min: 0.1,
+              max: 0.6,
+            },
+
+            random: true,
+
+            anim: {
+              enable: true,
+              speed: 0.3,
+              opacity_min: 0.1,
+              sync: false,
+            },
+          },
+
+          shadow: {
+            enable: true,
+            color: "#a855f7",
+            blur: 8,
+          },
+
+          size: {
+            value: {
+              min: 0.5,
+              max: 3,
+            },
+          },
+
+          move: {
+            enable: true,
+            speed: 0.3,
+            direction: "top-left",
+            random: true,
+            straight: false,
+
+            outModes: {
+              default: "out",
+            },
+          },
+        },
+      }}
+    />
+  )
+}
